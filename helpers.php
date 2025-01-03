d5fdc988fb58e8825206bfe0b9a32032a0577100
Francesco Miccio <mifr82@gmail.comgit config --global user.name Miccius>
2025-01-03 20:55:51 +0000

    Fix partials path issue and update helpers.php

--- helpers.php
+++ helpers.php
@@ -12,3 +12,61 @@
  {
    return __DIR__ . '/' . $path;
  }
+/**
+  * Load a vew
+  *@param string $name
+  *@return void
+
+  */
+
+  function loadView($name) 
+  {
+    $viewPath = basePath("views/{$name}.view.php");
+
+    //inspectAndDie($viewPath);
+
+    if(file_exists($viewPath)) {
+      require $viewPath;
+  } else {
+    echo "View '{$name} not found!'";
+  }
+
+  }
+
+ /**
+  * Load a vew
+  *@param string $name
+  *@return void
+
+  */
+
+  function loadPartial($name) 
+  {
+      $path = basePath("views/partials/{$name}.php");
+      if (file_exists($path)) {
+          require $path;
+      } else {
+          echo "Partial '{$name}' not found at {$path}";
+      }
+  }
+  
+
+  function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
