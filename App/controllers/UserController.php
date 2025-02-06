2af29df49cdfef66656e6fb6a5b4ee811fd69c88
Francesco Miccio <mifr82@gmail.comgit config --global user.name Miccius>
2025-02-06 14:41:17 +0000

    Authorize middleware

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -141,5 +141,70 @@ public function logout()
   redirect('/');
 }
 
+/**
+ * 
+ * @return void
+ */
+public function authenticate() {
+
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $errors = [];
+
+  //validation
+  if(!Validation::email($email)) {
+
+    $errors['email'] = 'Please enter a valid email';
+  }
+if(!Validation::string($password, 6, 50)) {
+
+  $errors['password'] = 'Passwoed must be at least 6 characters';
+
+  } 
+
+  //Check for errors
+if(!empty($errors)) {
+  loadView('users/login', ['errors' => $errors]);
+  exit;
+}
+
+//check for email
+$params = [
+  'email'=> $email
+];
+
+$user = $this->db->query('SELECT * FROM users WHERE `e-mail` = :email',$params)->fetch();
+
+if(!$user) {
+  $errors['email'] = 'Incorrect credentials';
+  loadView('users/login', ['errors' => $errors]);
+  exit;
+}
+
+//check if password is correct
+if(!password_verify($password, $user->password)) {
+  $errors['email'] = 'Incorrect credentials';
+  loadView('users/login', ['errors' => $errors]);
+  exit;
+}
+
+//Set user session
+Session::set('user', [
+  'id' => $user->Id,
+  'name' => $user->name,
+  'email' => $user->email,
+  'city' => $user->city,
+  'state' => $user->state
+]);
+
+
+redirect('/');
+
+}
+  
+ 
+
+
 
 }
--- App/views/users/login.view.php
+++ App/views/users/login.view.php
@@ -9,10 +9,11 @@
         <div class="message bg-green-100 p-3 my-3">
           This is a success message.
         </div> -->
+        <?= loadPartial('errors',['errors' => $errors ?? []])?>
         <form method="POST"  action="/auth/login">
           <div class="mb-4">
             <input
-              type="email"
+              type="text"
               name="email"
               placeholder="Email Address"
               class="w-full px-4 py-2 border rounded focus:outline-none"
